fbb8ec12c60763b826d2888c000366dc6ab6c52e
lu4nsds <luan.s9d7s@gmail.com>
2020-12-19 01:12:39 -0300

    Mudanças no grafíco - Victory-Bar

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,9 +1,7 @@
 import React, {useState} from 'react';
 import { ScrollView } from "react-native";
-import {
-  BarChart,
-  StackedBarChart,
-} from "react-native-chart-kit";
+import {VictoryBar, VictoryChart, VictoryGroup} from "victory-native";
+
 import {
     styles,
     
@@ -18,81 +16,38 @@ import {
 
 function Charts({hosp}){
 
-    
-    const chartConfig = {
-    backgroundGradientFrom: "#1E2923",
-    backgroundGradientFromOpacity: 0,
-    backgroundGradientTo: "#08130D",
-    backgroundGradientToOpacity: 0,
-    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-    barPercentage: 0.8,
-    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-    propsForBackgroundLines: {margin: 5},
-    
-    useShadowColorFromDataset: false, // optional
-
-
-    propsForBackgroundLines: {
-        stroke: '#ffffff',
-        strokeWidth: "0.5",
-        strokeDashOffset: 10,
-        scale: 0.75,
-    },
-
-    propsForVerticalLabels: {
-            fontSize: 15,
-            rotation: -45,
-            decimalPlaces: 0,
-            padding: 10,
-        },
-        
-    propsForHorizontalLabels: {
-            fontSize: 10,
-            decimalPlaces:1,
-            
-        
-        },
-    };
-
     const data = {
-        labels: ["jan", "fev", "mar","abr", "mai", "jun","jul", "ago", "set","out", "nov", "dez"],
-        legend:['seu cu', 'seu anus', 'seu boga'],
-        data: [
-            [30, 60, 60],
-            [30, 30, 60],
-            [30, 30, 60],
-            [30, 30, 60],
-            [30, 30, 60],
-            [30, 30, 60],
-            [30, 30, 60],
-            [30, 30, 60],
-            [30, 30, 60],
-            [30, 30, 60],
-            [30, 30, 60],
-            [30, 30, 60],
-        ],
-        barColors: ["#ffffff", "#555555", "#45cbf3"]
-        };
+        planned: [
+            {x: 'week 1', y: 50}],
+
+
+        actual: [],
+
+    };
+    
+
+    
 
     return (
         <View> 
             <ScrollView
-            style={{
-                backgroundColor: '#000000',
-                marginVertical: 0,
-                width: '100%',
-            }}
-            horizontal={true}
-            >
-                <StackedBarChart
-                style={styles.barChart}
-                data={data}
-                width={800}
-                height={300}             
-                chartConfig={chartConfig}
-                showLegend={true}
-                /* propsForHorizontalLabels={{justifyContent: 'space-between'}} */
-                />
+                style={{
+                    backgroundColor: '#000000',
+                    marginVertical: 0,
+                    width: '100%',
+                    marginTop: 50,
+                }}
+                horizontal={true}
+                >
+                    <VictoryChart>
+                        <VictoryGroup>
+                            <VictoryBar/>
+
+                            <VictoryBar/>
+                        </VictoryGroup>                     
+                   
+                    </VictoryChart>    
+                            
 
             </ScrollView>         
                       
